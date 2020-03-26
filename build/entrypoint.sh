a233734b4cb187834f7af3649e63326a3090f268
Ezekiel Keator <20361796+tastypackets@users.noreply.github.com>
2020-03-26 17:55:43 -0500

    wip

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -6,17 +6,18 @@ set -e
 : ${IMAGE:=$GITHUB_REPOSITORY}
 : ${ARGS:=} # Default: empty build args
 : ${TAG:=$GITHUB_SHA}
-: ${BRANCH:=''}
+: ${BRANCH_TAG:=true}
 : ${LATEST:=true}
 : ${WORKING_DIRECTORY:=.}
 
 docker build $ARGS -t $IMAGE:$TAG $WORKING_DIRECTORY
-docker tag $IMAGE:$tag $GCLOUD_REGISTRY/$IMAGE:$TAG # SHA or custom tag
+docker tag $IMAGE:$tag $GCLOUD_REGISTRY/$IMAGE:tag # SHA or custom tag
 
 if [ $LATEST = true ]; then
   docker tag $IMAGE:$TAG $GCLOUD_REGISTRY/$IMAGE:latests # Latest tag
 fi
 
-if [ -n "$BRANCH" ]; then
-  docker tag $IMAGE:$TAG $GCLOUD_REGISTRY/$IMAGE:$BRANCH # Branch tag
+if [ $BRANCH_TAG = true ]; then
+  BRANCH=$(echo $GITHUB_REF | rev | cut -f 1 -d / | rev)
+  docker tag $IMAGE:$TAG $GCLOUD_REGISTRY/$IMAGE:test # Branch tag
 fi
