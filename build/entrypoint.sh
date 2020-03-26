2dd6ff91e4c9bef67d94620481f539f39b3c64bf
Ezekiel Keator <20361796+tastypackets@users.noreply.github.com>
2020-03-26 18:22:40 -0500

    wip

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -10,11 +10,15 @@ set -e
 : ${LATEST:=true}
 : ${WORKING_DIRECTORY:=.}
 
+echo $GITHUB_SHA
+echo $GITHUB_REF
+echo $TAG
+
 docker build $ARGS -t $IMAGE:$TAG $WORKING_DIRECTORY
 docker tag $IMAGE:$TAG $GCLOUD_REGISTRY/$IMAGE:$TAG # SHA or custom tag
 
 if [ $LATEST = true ]; then
-  docker tag $IMAGE:$TAG $GCLOUD_REGISTRY/$IMAGE:latests # Latest tag
+  docker tag $IMAGE:$TAG $GCLOUD_REGISTRY/$IMAGE:latest # Latest tag
 fi
 
 if [ $BRANCH_TAG = true ]; then
