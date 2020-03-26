53561e70681b8bc7c4d13f2c666396c7ff153d98
Ezekiel Keator <20361796+tastypackets@users.noreply.github.com>
2020-03-26 18:54:32 -0500

    wip

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -5,13 +5,14 @@ set -e
 : ${GCLOUD_REGISTRY:=gcr.io}
 : ${IMAGE:=$GITHUB_REPOSITORY}
 : ${ARGS:=} # Default: empty build args
-: ${TAG:=$GITHUB_SHA}
+: ${TAG:=$GITHUB_REF} # Default tag to git SHA
 : ${BRANCH_TAG:=true}
 : ${LATEST:=true}
-: ${WORKING_DIRECTORY:=.}
+: ${WORKING_DIRECTORY:=.} # Default to CWD
 
 echo $GITHUB_SHA
 echo $GITHUB_REF
+echo $GITHUB_HEAD_REF
 echo $TAG
 
 docker build $ARGS -t $IMAGE:$TAG $WORKING_DIRECTORY
@@ -22,6 +23,11 @@ if [ $LATEST = true ]; then
 fi
 
 if [ $BRANCH_TAG = true ]; then
-  BRANCH=$(echo $GITHUB_REF | rev | cut -f 1 -d / | rev)
+  BRANCH_RAW = $GITHUB_REF
+  if [ -n "$GITHUB_HEAD_REF" ]; then
+    BRANCH_RAW = $GITHUB_HEAD_REF
+  fi
+  echo $BRANCH_RAW
+  BRANCH=$(echo $BRANCH_RAW | rev | cut -f 1 -d / | rev)
   docker tag $IMAGE:$TAG $GCLOUD_REGISTRY/$IMAGE:$BRANCH # Branch ta
 fi
